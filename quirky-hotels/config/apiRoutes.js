88ef764db2b3e08f47c223a9fdbc06863e74f03f
ebere10 <ebere10@hotmail.com>
2017-01-13 11:47:44 +0000

    corrected apiRoutes and added info to webRoutes file

--- quirky-hotels/config/apiRoutes.js
+++ quirky-hotels/config/apiRoutes.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const hotelsController = require('../controllers/hotels');
-const staticsController = require('../controllers/statics');
-
-router.route('/')
-  .get(staticsController.home);
 
 router.route('/hotels')
-    .get(hotelsController.index);
+  .get(hotelsController.index);
 
 //module exports
 module.exports = router;
