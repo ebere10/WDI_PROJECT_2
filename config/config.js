3200e401582aa6e24067875e94721c4e1eeda11b
ebere10 <ebere10@hotmail.com>
2017-01-18 10:08:41 +0000

    Added process.env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGOBD_URI || 'mongodb://localhost/quirky-hotels',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/quirky-hotels',
   secret: process.env.SECRET || 'quirky-hotels'
 };
