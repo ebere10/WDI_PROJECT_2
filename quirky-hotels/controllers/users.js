df9f6f959052cbe557635d383c522b509677c8f4
ebere10 <ebere10@hotmail.com>
2017-01-18 08:15:58 +0000

    deleting unecessary functions from user model

--- quirky-hotels/controllers/users.js
+++ quirky-hotels/controllers/users.js
@@ -7,33 +7,6 @@ function usersIndex(req, res) {
   });
 }
 
-function usersShow(req, res) {
-  User.findById(req.params.id, (err, user) => {
-    if (err) return res.status(500).json({ message: 'Something went wrong'});
-    if (!user) return res.status(404).json({ message: 'User not found'});
-    return res.status(200).json({ user});
-  });
-}
-
-function usersUpdate(req, res) {
-  User.findByIdAndUpdate(req.params.id, req.body.user, { new: true}, (err, user) => {
-    if (err) return res.status(500).json({ mesage: 'Something went wrong'});
-    if (!user) return res.status(404).json({ message: 'User not found'});
-    return res.status(200).json({ user});
-  });
-}
-
-function usersDelete(req, res) {
-  User.findByIdAndRemove(req.params.id, (err, user) => {
-    if (err) return res.status(500).json({ message: 'Something went wrong'});
-    if (!user) return res.status(404).json({ message: 'User not found'});
-    return res.status(200).send();
-  });
-}
-
 module.exports = {
-  index: usersIndex,
-  show: usersShow,
-  update: usersUpdate,
-  delete: usersDelete
+  index: usersIndex
 };
